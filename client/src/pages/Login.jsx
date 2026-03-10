51b0dd6c6355b8df7f61e8ec8e5e21cf70c7d64c
Hardik Srivastava <hardiksatwik2004@gmail.com>
2026-03-10 18:56:56 +0530

    update

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div className="min-h-screen flex flex-col md:flex-row">
+            {/* Background Image */}
+            
+            
+
+        </div>
+    )
+}
+
+export default Login
