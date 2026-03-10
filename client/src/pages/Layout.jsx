e70b85102a268ac953ad18c87d70ca0260025a55
Hardik Srivastava <hardiksatwik2004@gmail.com>
2026-03-11 00:43:26 +0530

    dfssdf

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../components/Sidebar'
 import { Outlet } from 'react-router-dom'
 import { Menu, X } from 'lucide-react'
 import { dummyUserData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const Layout = () => {
 
@@ -11,18 +12,20 @@ const Layout = () => {
 
   return user ? (
     <div className='w-full flex h-screen'>
-      <Sidebar />
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className='flex-1 bg-slate-50'>
         <Outlet />
       </div>
       {
-         sidebarOpen ?
-        <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' />
+        sidebarOpen ?
+        <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=> setSidebarOpen(false)}/>
         :
-        <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden'/>
+        <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=> setSidebarOpen(true)}/>
       }
-    </div>
-  )   
+      </div>
+  ) : (
+    <Loading />
+  )  
 }
 
 export default Layout
