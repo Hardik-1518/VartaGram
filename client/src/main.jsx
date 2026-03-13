656aa85a6c7cd02e0313b80b76fe5665eeac48bc
Hardik Srivastava <hardiksatwik2004@gmail.com>
2026-03-13 16:11:47 +0530

    updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'  
+import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/react'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ClerkProvider>,
+  <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ClerkProvider>
 )
-
