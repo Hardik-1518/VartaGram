556f6cb96a490b4101def37dfe26fe93d1dc4501
Hardik Srivastava <hardiksatwik2004@gmail.com>
2026-06-03 17:07:26 +0530

    mobile view updates

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -21,9 +21,9 @@ const Layout = () => {
       <Sidebar />
 
       <div className='flex-1 bg-slate-50 pb-20 sm:pb-0'>
-        <div className='sm:hidden sticky top-0 z-20 border-b border-gray-200 bg-white px-4 py-3'>
-          <Link to='/' className='inline-flex items-center gap-2'>
-            <img src={assets.logo} alt='VartaGram Logo' className='h-12 object-contain' />
+        <div className='sm:hidden sticky top-0 z-20 border-b border-gray-200 bg-white px-4 py-2'>
+          <Link to='/' className='flex items-center justify-center'>
+            <img src={assets.logo} alt='VartaGram Logo' className='h-10 object-contain' />
           </Link>
         </div>
 
